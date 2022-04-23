9ede7f00850e8a6b2da56a936d87f442364ca520
Dacee-dee <75333265+Dacee-dee@users.noreply.github.com>
2022-04-24 00:13:39 +0530

    The counter pro-whatever. Still getting used to CSS, but it's a start!

---
+++ counter/Script.js
@@ -0,0 +1,13 @@
+let count = 0;
+document.getElementById("decreaseButton").onclick = function () {
+    count--;
+    document.getElementById("count").innerHTML= count;
+}
+document.getElementById("resetButton").onclick = function () {
+    count = 0;
+    document.getElementById("countLabel").innerHTML = count;
+}
+document.getElementById("increaseButton").onclick = function () {
+    count++;
+    document.getElementById("count").innerHTML = count;
+}
